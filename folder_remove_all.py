cdba10010b1164b883af60fecd6cba9d6f977f58
jreadey <jreadey@hdfgroup.org>
2020-10-26 19:16:53 -0700

    add folder_remove_all.py

---
+++ folder_remove_all.py
@@ -0,0 +1,44 @@
+import sys
+import h5pyd
+
+#
+# Remove all domains in given folder
+#
+def printUsage():
+    print("python folder_remove_all.py [--bucket bucketname] <folder>")
+    print("Removes all domains in give folder!")
+    sys.exit(0)
+
+
+def main():
+    if len(sys.argv) == 1 or (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+        printUsage()
+        sys.exit(1)
+    
+    bucket = None
+
+    if len(sys.argv) == 4 and sys.argv[1] == "--bucket":
+        bucket = sys.argv[2]
+        print("using bucket:", bucket)
+    
+    folder_path = sys.argv[-1]
+    if folder_path[0] != '/' or folder_path[-1] != '/':
+        print("domain folder must start and end with '/")
+        printUsage()
+
+    print("delete:", folder_path)
+
+    folder = h5pyd.Folder(folder_path, mode='r+')
+    domains = []
+    for domain in folder:
+        print(domain)
+        domains.append(domain)
+
+    for domain in domains:
+        print(f"removing: {domain}")
+        del folder[domain]
+
+main()
+
+
+
