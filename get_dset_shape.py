e8938a5e3254a5e9cb6c59d000ad8f01c2b6b26f
jreadey <jreadey@hdfgroup.org>
2020-12-15 10:57:06 -0800

    add get shape util

---
+++ get_dset_shape.py
@@ -0,0 +1,52 @@
+import sys
+import s3fs
+import logging
+
+USE_H5PY=1
+if USE_H5PY:
+    import h5py
+else:
+    import h5pyd as h5py
+
+
+def visit(name):
+    dset = f[name]
+    if not isinstance(dset, h5py.Dataset):
+        # skip groups
+        return None
+     
+    shape = dset.shape
+    logging.info(f'{name} got shape: {shape}')
+    shape_map[name] = shape
+    return None
+
+#
+# Main
+#
+
+loglevel = logging.INFO
+logging.basicConfig(format='%(asctime)s %(message)s', level=loglevel)
+shape_map = {}
+
+if len(sys.argv) < 2 or sys.argv[1] in ("-h", "--help"):
+    print("usage: python get_dset_means.py <domain>")
+    sys.exit(0)
+
+filename = sys.argv[1]
+if filename.startswith("s3://"):
+    s3 = s3fs.S3FileSystem()
+    f = h5py.File(s3.open(filename, "rb"), mode='r')
+else:
+    f = h5py.File(filename, mode="r", use_cache=False)
+f.visit(visit)
+names = list(shape_map.keys())
+names.sort()
+for name in names:
+    shape = shape_map[name]
+    print(f"{name}: {shape}")
+
+ 
+
+
+
+
